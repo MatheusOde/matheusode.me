a9523473e6ba415102e5da72e0045f26a3d8e506
MatheusOde <matheusode@gmail.com>
2026-02-05 11:47:04 -0300

    slight changes to url

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/matheusode.me/">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </StrictMode>,
